e111c743ef7b4a7ee80484195ebbc7e2b21a00b1
Nirzar <93899190+nirzartelga@users.noreply.github.com>
2023-09-30 10:40:52 +0530

    Project created.

---
+++ Practical_Unlimit/Practical_UnlimitUITests/Practical_UnlimitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Practical_UnlimitUITestsLaunchTests.swift
+//  Practical_UnlimitUITests
+//
+//  Created by Nirzar Gandhi on 30/09/23.
+//
+
+import XCTest
+
+final class Practical_UnlimitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
